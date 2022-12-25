3737f2be6f49f6b830145a79b2598d488637a39f
VuMinhQuanDev <vuminhquan2904@gmail.com>
2022-12-25 11:17:39 +0700

    change config json3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'E:/chat-app-sic/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Login from './login'
 import { useAuthState } from 'react-firebase-hooks/auth'
